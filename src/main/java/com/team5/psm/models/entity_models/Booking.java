dc2dee3615a82ccb565c5d35573eb33eeecb3505
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 00:01:29 +0700

    Booking Entity #46

---
+++ src/main/java/com/team5/psm/models/entity_models/Booking.java
@@ -0,0 +1,42 @@
+package com.team5.psm.models.entity_models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    @ManyToMany
+    @JoinColumn(name = "service_id")
+    private List<Service> service;
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDateTime created_date;
+
+    @OneToOne(mappedBy = "book")
+    private Feedback feedback;
+
+}
