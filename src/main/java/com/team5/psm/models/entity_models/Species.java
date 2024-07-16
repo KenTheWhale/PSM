98a8d4d674408554d4a6fcfd23364291462da88b
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 00:02:18 +0700

    Species Entity #46

---
+++ src/main/java/com/team5/psm/models/entity_models/Species.java
@@ -0,0 +1,30 @@
+package com.team5.psm.models.entity_models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Species {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "specie")
+    private List<Pet> pets;
+
+}
