90f9b19ef0dcc11bb6521fbfbe6498d4b024626a
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-07-16 19:07:52 +0700

    Register request

--- src/main/java/com/team5/psm/requests/RegisterRequest.java
+++ src/main/java/com/team5/psm/requests/RegisterRequest.java
@@ -1,5 +1,17 @@
 package com.team5.psm.requests;
 
-public class RegisterRequest {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
 }
