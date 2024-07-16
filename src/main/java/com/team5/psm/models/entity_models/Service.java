4e008874fb3a4286ae8a91355add16c82b8538e9
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 00:01:58 +0700

    Service Entity #46

---
+++ src/main/java/com/team5/psm/models/entity_models/Service.java
@@ -0,0 +1,44 @@
+package com.team5.psm.models.entity_models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "status_id")
+    private Service_Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "center_id")
+    private Spa_Center center;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private double price;
+
+    @ManyToMany(mappedBy = "service")
+    private List<Booking> bookings;
+
+}
