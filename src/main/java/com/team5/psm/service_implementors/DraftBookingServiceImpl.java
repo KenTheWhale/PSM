b85b265c5b35f481960bb1e7a384becee0ec0915
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 20:41:00 +0700

    #27 Create DraftBooking #48 Update DraftBooking

---
+++ src/main/java/com/team5/psm/service_implementors/DraftBookingServiceImpl.java
@@ -0,0 +1,69 @@
+package com.team5.psm.service_implementors;
+
+import com.team5.psm.models.entity_models.DraftBooking;
+import com.team5.psm.models.entity_models.Pet;
+import com.team5.psm.models.entity_models.Services;
+import com.team5.psm.models.request_models.CreateDraftBookingRequest;
+import com.team5.psm.models.request_models.UpdateDraftBookingRequest;
+import com.team5.psm.repositories.DraftBookingRepo;
+import com.team5.psm.repositories.PetRepo;
+import com.team5.psm.repositories.ServicesRepo;
+import com.team5.psm.repositories.SpaCenterRepo;
+import com.team5.psm.services.DraftBookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+
+@Service
+@RequiredArgsConstructor
+public class DraftBookingServiceImpl implements DraftBookingService {
+
+    private final DraftBookingRepo draftBookingRepo;
+    private final ServicesRepo servicesRepo;
+    private final PetRepo petRepo;
+    private final SpaCenterRepo spaCenterRepo;
+
+    @Override
+    public String createDraftBooking(CreateDraftBookingRequest request, Model model) {
+        String petName = request.getPetName();
+        String spaName = request.getSpaName();
+        String serviceName = request.getServiceName();
+        float price = request.getPrice();
+
+        Pet pet = petRepo.findByName(petName).orElse(null);
+        Services services = servicesRepo.findByNameAndSpaCenter(serviceName, spaCenterRepo.findByName(spaName).orElse(null)).orElse(null);
+
+        DraftBooking draftBooking = DraftBooking.builder()
+                                                .pet(pet)
+                                                .service(services)
+                                                .price(price)
+                                                .build();
+        model.addAttribute("draftBooking", draftBooking);
+
+        draftBookingRepo.save(draftBooking);
+        return "home";
+    }
+
+    @Override
+    public String updateDraftBooking(UpdateDraftBookingRequest request, Model model) {
+        String petName = request.getPetName();
+        String spaName = request.getSpaName();
+        String serviceName = request.getServiceName();
+        float price = request.getPrice();
+
+        DraftBooking draftBooking = draftBookingRepo.findById(request.getId()).orElse(null);
+        Pet pet = petRepo.findByName(petName).orElse(null);
+        Services services = servicesRepo.findByNameAndSpaCenter(serviceName, spaCenterRepo.findByName(spaName).orElse(null)).orElse(null);
+
+        draftBooking = DraftBooking.builder()
+                                    .pet(pet)
+                                    .service(services)
+                                    .price(price)
+                                    .build();
+        model.addAttribute("draftBooking", draftBooking);
+
+        draftBookingRepo.save(draftBooking);
+        return "";
+    }
+}
