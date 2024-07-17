5348faae4999a31feaee5b19cc050be4ebfa4857
KenTheWhale <kienquoc11026789@gmail.com>
2024-07-18 02:49:03 +0700

    add base

--- src/main/java/com/team5/psm/consts/Role.java
+++ src/main/java/com/team5/psm/consts/Role.java
@@ -4,22 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class Role {
-    public static String USER = "User";
-    public static String SHOP_OWNER = "Shop Owner";
-    public static String ADMIN = "Admin";
-
-//    private final String value;
-//
-//    ERoleEnum(String value) {
-//        this.value = value;
-//    }
-//
-//    public static ERoleEnum fromValue(String value) {
-//        for (ERoleEnum role : ERoleEnum.values()) {
-//            if (role.getValue().equals(value)) {
-//                return role;
-//            }
-//        }
-//        return null;
-//    }
+    public static String USER = "user";
+    public static String SHOP = "shop";
 }
