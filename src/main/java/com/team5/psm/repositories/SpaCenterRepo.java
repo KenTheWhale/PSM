2f7e378c02b99ccf18fb4da7ffe1b71cba38672d
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 20:41:48 +0700

    find spaCenter by name

--- src/main/java/com/team5/psm/repositories/SpaCenterRepo.java
+++ src/main/java/com/team5/psm/repositories/SpaCenterRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team5.psm.models.entity_models.SpaCenter;
 
+import java.util.Optional;
+
 public interface SpaCenterRepo extends JpaRepository<SpaCenter, Long> {
+
+    Optional<SpaCenter> findByName(String name);
 
 }
