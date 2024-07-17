21753445f2f0282ee0e2e2de656cc6998985fb15
KenTheWhale <kienquoc11026789@gmail.com>
2024-07-18 01:47:36 +0700

    add server database

--- src/main/java/com/team5/psm/services/UserService.java
+++ src/main/java/com/team5/psm/services/UserService.java
@@ -3,12 +3,6 @@ package com.team5.psm.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-
-import com.team5.psm.models.entity_models.Services;
 
 import com.team5.psm.models.entity_models.User;
 import com.team5.psm.models.request_models.UpdateProfileRequest;
@@ -23,6 +17,6 @@ public interface UserService {
 	
 	String updateProfile(UpdateProfileRequest request, Model model, Long id);
   
-  void loadHomePage(Model model);
+	void loadHomePage(Model model);
 	
 }
