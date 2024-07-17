5a1a5c78ac10dc339a1d7ef463a12bae406e9209
ToLazyToCode <hungphucvp7@gmail.com>
2024-07-17 20:43:04 +0700

    find pet by name

--- src/main/java/com/team5/psm/repositories/PetRepo.java
+++ src/main/java/com/team5/psm/repositories/PetRepo.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team5.psm.models.entity_models.Pet;
 
+import java.util.Optional;
+
 public interface PetRepo extends JpaRepository<Pet, Long> {
+
+    Optional<Pet> findByName(String name);
 
 }
