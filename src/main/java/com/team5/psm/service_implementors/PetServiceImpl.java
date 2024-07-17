ea8d8c20a49c0ac394a1a2adb35f5a1671364e23
HuyTran0211 <quochuy2112002@gmail.com>
2024-07-18 03:09:33 +0700

    add funtion for pet

--- src/main/java/com/team5/psm/service_implementors/PetServiceImpl.java
+++ src/main/java/com/team5/psm/service_implementors/PetServiceImpl.java
@@ -10,6 +10,7 @@ import com.team5.psm.models.entity_models.Pet;
 import com.team5.psm.models.entity_models.Species;
 import com.team5.psm.models.entity_models.User;
 import com.team5.psm.models.request_models.AddPetRequest;
+import com.team5.psm.models.request_models.UpdatePetRequest;
 import com.team5.psm.models.request_models.ViewAllPetOfUserRequest;
 import com.team5.psm.repositories.AccountRepo;
 import com.team5.psm.repositories.PetRepo;
@@ -23,11 +24,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PetServiceImpl implements PetService {
 	private final PetRepo petRepo;
-	
+
 	private final UserRepo userRepo;
-	
+
 	private final SpeciesRepo speciesRepo;
-	
+
 	private final AccountRepo accountRepo;
 
 	@Override
@@ -37,31 +38,43 @@ public class PetServiceImpl implements PetService {
 		return petList;
 
 	}
+
 	@Override
-	public String AddPet(AddPetRequest request, Long accID, Long speciesID, Model model) {
-	    String petName = request.getName();
-	    String description = request.getDescription();
-	    
-	    if (!checkIfStringIsValid(petName)) {
-	        model.addAttribute("error", "Please fill in Pet Name.");
-	        return "pet";
-	    }
-	    
-	    Species species = speciesRepo.findById(speciesID).orElse(null);
-	            
-	    User user = userRepo.findByAccount_Id(accID);
-	    
-	    Pet pet = Pet.builder()
-	            .name(petName)
-	            .description(description)
-	            .species(species)
-	            .user(user)
-	            .build();
-	    petRepo.save(pet);
-	    return "redirect:/pet";
+	public String addPet(AddPetRequest request, Long accID, Long speciesID, Model model) {
+		String petName = request.getName();
+		String description = request.getDescription();
+
+		if (!checkIfStringIsValid(petName)) {
+			model.addAttribute("error", "Please fill in Pet Name.");
+			return "pet";
+		}
+
+		Species species = speciesRepo.findById(speciesID).orElse(null);
+
+		User user = userRepo.findByAccount_Id(accID);
+
+		Pet pet = Pet.builder().name(petName).description(description).species(species).user(user).build();
+		petRepo.save(pet);
+		return "redirect:/pet";
+	}
+
+	@Override
+	public String updatePet(UpdatePetRequest request, Model model) {
+		Long petId = request.getId();
+		Pet pet = petRepo.findById(petId).orElse(null);
+		if (pet == null) {
+			model.addAttribute("error", "Pet not found");
+			return "pet";
+		}
+		pet.setName(request.getName());
+		pet.setDescription(request.getDescription());
+
+		petRepo.save(pet);
+		return "redirect:/pet";
 	}
 
 	private boolean checkIfStringIsValid(String input) {
 		return input != null && !input.isEmpty();
 	}
+
 }
