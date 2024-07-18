bd998ea3776e3ca2973bea2862652996307e6044
HuyTran0211 <quochuy2112002@gmail.com>
2024-07-18 12:51:06 +0700

    add function and button deleteBooking

---
+++ src/main/java/com/team5/psm/models/request_models/CancelBookingRequest.java
@@ -0,0 +1,14 @@
+package com.team5.psm.models.request_models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CancelBookingRequest {
+	Long BookingId;
+}
